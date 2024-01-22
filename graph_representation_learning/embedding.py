af57e0ca6ddedc35aae04b67b3d6b48a883ca654
sander-hergarten <mail@sander-hergarten.de>
2024-01-22 12:30:25 +0100

    DEV:added basic model embedding

--- graph_representation_learning/embedding.py
+++ graph_representation_learning/embedding.py
@@ -1,3 +1,17 @@
+from ..utils import config
+import tensorflow as tf
+import os
+from pathlib import Path
+
+SOURCE_DIR = os.environ["SOURCE_DIR"]
+model_path = Path(SOURCE_DIR) / config["embedding_model"] / config["dataset"]
+
 
 def from_observations(observations):
-    ...
+    model = _load_model_from_file()
+    embeddings = model.embed(observations)
+    return embeddings
+
+
+def _load_model_from_file() -> tf.keras.Model:
+    return tf.keras.models.load_model(model_path)
