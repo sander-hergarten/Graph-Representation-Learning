dac73d964a9ad02b1b19720881a58bab3797e807
sander-hergarten <mail@sander-hergarten.de>
2024-06-25 13:00:43 +0200

    bunch of stuff

--- agent.py
+++ agent.py
@@ -1,9 +1,110 @@
 import torch
+from torch import nn
+import torch.optim as optim
+import torch.nn.functional as F
+from collections import namedtuple, deque
 import gymnasium as gym
+import random
+import math
 
 import environment
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-env = gym.make(environment.GraphLearnerEnv(node_data_size, embedding_size, render_mode))
+node_data_size = 10
+embedding_size = 32
 
 
+env_name = environment.register_env(node_data_size, embedding_size, "none")
+env = gym.make(env_name)
+
+Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN(nn.Module):
+
+    def __init__(self, node_data_size, embedding_size, hidden_size):
+        super(DQN, self).__init__()
+        self.gru_head_1 = nn.GRUCell(input_size=node_data_size, hidden_size=hidden_size)
+        self.gru_head_2 = nn.GRUCell(input_size=node_data_size, hidden_size=hidden_size)
+
+        self.hidden_layer = nn.Linear(256 + embedding_size, hidden_size)
+
+        self.out_head_1 = nn.Linear(hidden_size, node_data_size)
+        self.out_head_1 = nn.Linear(hidden_size, node_data_size)
+
+    def forward(self, edges, embedding):
+        h1 = torch.zeros(128, device=device)
+        h2 = torch.zeros(128, device=device)
+
+        for edge in edges:
+            h1 = self.gru_head_1(edge[0], h1)
+
+        for edge in edges:
+            h2 = self.gru_head_2(edge[1], h2)
+
+        h = torch.cat((h1, h2, embedding), 1)
+        h = F.relu(self.hidden_layer(h))
+
+        out_node_1 = F.sigmoid(self.out_head_1(h))
+        out_node_2 = F.sigmoid(self.out_head_2(h))
+
+        return out_node_1, out_node_2
+
+
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+
+state, info = env.reset()
+
+
+policy_net = DQN(node_data_size, embedding_size, 64).to(device)
+target_net = DQN(node_data_size, embedding_size, 64).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(10000)
+
+
+steps_done = 0
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(
+        -1.0 * steps_done / EPS_DECAY
+    )
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1).indices.view(1, 1)
+    else:
+        return torch.tensor(
+            [[env.action_space.sample()]], device=device, dtype=torch.long
+        )
--- environment.py
+++ environment.py
@@ -2,26 +2,42 @@ import gymnasium
 from gymnasium.spaces import Dict, Box, Tuple, Space, Sequence
 import numpy as np
 import rustworkx as rx
-from typing import TypeVar, Any, TypedDict 
+from rustworkx.visualization import graphviz_draw
+from typing import  Any, TypedDict, Optional
 import torch
+import time 
+import os
+from pathlib import Path
 from torch import nn
+import ffmpeg
 
 class ActType(TypedDict):
     edges: tuple[torch.Tensor, torch.Tensor]
     embedd: torch.Tensor
 
-ObsType = TypeVar("ActType")
-RewardType = TypeVar("RewardType")
+
+ObsType = list[tuple[torch.Tensor, torch.Tensor]]
+RewardType = torch.Tensor
 Terminated = bool
 Truncated = bool
 Info = dict[str, Any]
 Done = bool
 
-CollectionBuffer = list[tuple[rx.PyDiGraph, list[torch.Tensor]]]
+CollectionBuffer = list[tuple[rx.PyDiGraph, torch.Tensor]]
 
-def get_edge_list_data(graph:rx.PyDiGraph):
+
+def images_directory_to_mp4(images_path:Path, out_path:Path = Path.cwd(), framerate: int = 10):
+    (
+        ffmpeg
+        .input(images_path / '*.jpg', pattern_type='glob', framerate=framerate)
+        .output(out_path / 'movie.mp4')
+        .run()
+    )
+
+
+def get_edge_list_data(graph: rx.PyDiGraph) -> ObsType:
     edge_list_indices = graph.edge_list()
-    return[(graph[node_0], graph[node_1]) for node_0, node_1 in edge_list_indices]
+    return [(graph[node_0], graph[node_1]) for node_0, node_1 in edge_list_indices]
 
 
 def _quotient_graph_edges(partition, graph):
@@ -30,30 +46,27 @@ def _quotient_graph_edges(partition, graph):
         for index in block:
             nodes[index] = graph[block[-1]]
 
-
     quotient_graph = rx.PyDiGraph()
     quotient_graph.add_nodes_from(nodes)
     quotient_graph.add_edges_from_no_data(graph.edge_list())
-
 
     for block in partition:
         for node_index in block[:-1]:
             quotient_graph.merge(node_index, block[-1])
 
-    
-    edge_list = get_edge_list_data(quotient_graph) 
+    edge_list = get_edge_list_data(quotient_graph)
 
     return edge_list
-        
 
-class Decoder(nn.module):
+
+class Decoder(nn.Module):
     def __init__(self, recurrent_layer_size, input_size, output_size):
         super().__init__()
-        self.hidden_initializer = lambda batch_size: torch.randn()
+        self.hidden_initializer = lambda batch_size: torch.randn(batch_size)
         self.gru_cell = nn.GRUCell(input_size, recurrent_layer_size)
         self.linear = nn.Linear(recurrent_layer_size, output_size)
         self.relu = nn.ReLU()
-    
+
     def forward(self, input):
         hidden = self.hidden_initializer(1)
         for element in input:
@@ -64,65 +77,71 @@ class Decoder(nn.module):
         return embedd
 
 
-class GraphLearnerEnv(gymnasium.env):
+class GraphLearnerEnv(gymnasium.Env):
     graph: rx.PyDigraph
     decoder: Decoder
     record: list[CollectionBuffer]
 
-    def __init__(self, node_data_size, embedding_size, render_mode, decoder_learning_rate = 1e-3):
+    def __init__(
+        self, node_data_size, embedding_size, render_mode, decoder_learning_rate=1e-3
+    ):
         super().__init__()
         mse = nn.MSELoss()
 
+        self.input_shape = node_data_size
+        self.loss_function = lambda y_true, y_pred: torch.sqrt(mse(y_true, y_pred))
 
-        # ?
-        self.input_shape= node_data_size
-        self.loss_function = lambda y_true, y_pred: torch.sqrt(mse[y_true, y_pred])
-        #?/
         self.decoder = Decoder(64, node_data_size, embedding_size)
-        self.optimizer = torch.optim.Adam(self.decoder.parameters(), lr=decoder_learning_rate)
+        self.optimizer = torch.optim.Adam(
+            self.decoder.parameters(), lr=decoder_learning_rate
+        )
 
         self.node_data_size = node_data_size
         self.embedding_size = embedding_size
         self.render_mode = render_mode
 
     @property
-    def action_space(self) -> Space[ActType]:
+    def action_space(self):
         node_data_space = Box(low=0, high=1, shape=self.node_data_size)
         edge_data_space = Tuple((node_data_space, node_data_space))
-        return Dict({
-            "edges": edge_data_space,
-            "embedd": Box(low=-np.inf, high = np.inf, shape=self.embedding_size)
-        })
+        return Dict(
+            {
+                "edges": edge_data_space,
+                "embedd": Box(low=-np.inf, high=np.inf, shape=self.embedding_size),
+            }
+        )
 
     @property
-    def observation_space(self) -> Space[ObsType]:
+    def observation_space(self):
         node_data_space = Box(low=0, high=1, shape=self.node_data_size)
         edge_data_space = Tuple((node_data_space, node_data_space))
         edge_sequence_space = Sequence(edge_data_space)
 
-        return edge_sequence_space 
+        return edge_sequence_space
 
     def metadata(self) -> dict[str, Any]:
         return {"render_modes": ["human"]}
 
-    def step(self, action: ActType) -> tuple[ObsType, RewardType, Terminated, Truncated, Info]:
-                
+    def step(
+        self, action: ActType
+    ) -> tuple[ObsType, RewardType, Terminated, Truncated, Info]:
+
         nodes = []
         for node_payload in action["edges"]:
-            if not (node:=self.graph.find_node_by_weight(node_payload)):
+            if not (node := self.graph.find_node_by_weight(node_payload)):
                 node = self.graph.add_node(node_payload)
 
             nodes.append(node)
 
-        self.graph.add_edge(nodes[0], node[1], None)
+        self.graph.add_edge(nodes[0], nodes[1], None)
 
         partition = rx.digraph_maximum_bisimulation(self.graph)
-        edge_list = _quotient_graph_edges(partition, self.graph) 
+        edge_list = _quotient_graph_edges(partition, self.graph)
 
         embedding = self.decoder(edge_list)
         loss = self.loss_function(action["embedd"], embedding)
 
-        reward = 1/loss
+        reward = 1 / loss
         observation = get_edge_list_data(self.graph)
         terminated = False
         truncated = False
@@ -131,9 +150,10 @@ class GraphLearnerEnv(gymnasium.env):
         self.record[-1].append((self.graph, action["embedd"]))
 
         return (observation, reward, terminated, truncated, info)
-    
-    def train_decoder(self, data, collection:int = -1):
+
+    def train_decoder(self, collection: int = -1):
         collection_buffer = self.record[collection]
+        loss = None
 
         for sample in collection_buffer:
             edge_list = get_edge_list_data(sample[0])
@@ -143,28 +163,49 @@ class GraphLearnerEnv(gymnasium.env):
 
             self.optimizer.step()
 
-        return loss 
+        if not loss:
+            print(f"WARNING: trained on empty collection {collection}")
+
+        return loss
+
+    def produce_images(self, produced_frames: int, collection: int = -1):
+        collection_buffer = self.record[collection][produced_frames:]
+
+        images = []
+        
+        for frame in collection_buffer:
+            images.append(graphviz_draw(frame[0]))
+
+        return images
+
+    def save_image_sequence(self, images, path, run_name:Optional[str] =None):
+        directory_name = f"{run_name if run_name else time.time()}"
+        path = Path(path)/directory_name
+        os.makedirs(directory_name)
+
+        for index, image in enumerate(images):
+            image.save(path / str(index))
 
 
     def add_collection_buffer(self):
         self.record.append([])
-        
-        
+
     def reset(self):
         super().reset(seed=None)
-        self.graph = rx.PyDiGraph()        
+        self.graph = rx.PyDiGraph()
 
         return ([], {})
-    
 
     def render(self):
-        raise NotImplementedError() 
+        raise NotImplementedError()
+
 
 # Register env
-def register_env(node_data_size, embedding_size, render_mode)-> str:
-    env_name= ""
-    gymnasium.envs.registration.EnvSpec(id = env_name, entrypoint =lambda : GraphLearnerEnv(node_data_size, embedding_size, render_mode))
-    
+def register_env(node_data_size, embedding_size, render_mode) -> str:
+    env_name = f"GraphLearner-{node_data_size}-{embedding_size}-{register_env}"
+    gymnasium.register(
+        id=env_name,
+        entrypoint=lambda: GraphLearnerEnv(node_data_size, embedding_size, render_mode),
+    )
+
     return env_name
-
-
